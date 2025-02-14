2db15dc8d20d0ccb849eeaf14eabfb45587ca72f
EvenCode21 <evencode21@gmail.com>
2025-02-14 11:20:33 -0400

    Project completed

---
+++ Events/Event.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace github_activity.Events
+{
+    public class Event
+    {
+        public record Payload(string Action, string Ref_type, int Size);
+        public record Repository(int Id, string Name, string Url);
+
+        public record Actor(int id, string login);
+
+        public string Id { get; set; }
+        public string Type { get; set; }
+
+        public Actor actor { get; set; }
+
+        public Repository Repo { get; set; }
+
+        public Payload payload { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,36 +1,60 @@
 ﻿using System.Text.Json;
 using System.Net.Http.Headers;
+using github_activity.Events;
 
 namespace github_activity;
 
 class Program
 {
-
-    static void Main(string[] args)
+    enum ExitCodes
+    {
+        Succes = 0,
+        UsernameMissing,
+        ApiError,
+        NoEvents
+    }
+    static int Main(string[] args)
     {
         HttpClient client = new HttpClient();
 
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Usage: github-activity <username>");
+            return (int) ExitCodes.UsernameMissing;
+        }
+
         client.BaseAddress = new Uri($"https://api.github.com/users/{args[0]}/");
-
-        var events = GetEvents(client).Result;
-
-        PrintEvents(events);
-
+        
+        if (client.DefaultRequestHeaders.UserAgent.TryParseAdd(args[0]))
+        {
+            var events = GetData(client,"events").Result;
+            
+            if (events.Length == 0)
+            {
+                Console.WriteLine("No activities found for this user");
+                return (int) ExitCodes.NoEvents;
+            }
+            
+            PrintEvents(events);
+            return (int) ExitCodes.Succes;
+        }
+        else
+        {
+            return (int) ExitCodes.ApiError;
+        }
     }
 
-    static async Task<Event[]> GetEvents(HttpClient client)
+    static async Task<Event[]> GetData(HttpClient client,string endPoint)
     {
         // headers required for the github api
-        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-        client.DefaultRequestHeaders.UserAgent.TryParseAdd("request");
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/vnd.github+json"));
 
-        HttpResponseMessage responseBody = client.GetAsync("events").Result;
-
+        HttpResponseMessage responseBody = client.GetAsync(endPoint).Result.EnsureSuccessStatusCode();
+        
         // throw an error if the status code is not successful
-        responseBody.EnsureSuccessStatusCode();
-
+        
         var response = await responseBody.Content.ReadAsStringAsync();
-
+        
 
         var options = new JsonSerializerOptions
         {
@@ -38,7 +62,7 @@ class Program
         };
 
         var root = JsonSerializer.Deserialize<Event[]>(response, options);
-
+        
         return root;
     }
 
@@ -46,8 +70,67 @@ class Program
     {
         foreach (Event e in events)
         {
-            Console.WriteLine(e);
+            switch (e.Type)
+            {
+                case "CreateEvent":
+                    if (e.payload.Ref_type == "repository")
+                    {
+                        Console.WriteLine($"- Created a new repository: {e.Repo.Name}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"- Created a new {e.payload.Ref_type} in {e.Repo.Name}");
+                    }
+                    break;
+                case "DeleteEvent":
+                    Console.WriteLine($"- Deleted a {e.payload.Ref_type} in {e.Repo.Name}");
+                    break;
+                case "ForkEvent":
+                    Console.WriteLine($"- Forked {e.Repo.Name}");
+                    break;
+                case "CommitCommentEvent":
+                    Console.WriteLine($"- Created a Commit comment in {e.Repo.Name}");
+                    break;
+                case "GollumEvent":
+                    break;
+                case "IssuesEvent":
+                    if (e.payload.Action == "opened")
+                    {
+                        Console.WriteLine($"- Opened a new issue in {e.Repo.Name}");
+                    }
+                    else if (e.payload.Action == "edited")
+                    {
+                        Console.WriteLine($"- Edited an issue in {e.Repo.Name}");
+                    }
+                    else if (e.payload.Action == "closed")
+                    {
+                        Console.WriteLine($"- Closed an issue in {e.Repo.Name}");
+                    }
+                    break;
+                case "PublicEvent":
+                    Console.WriteLine($"- Made public {e.Repo.Name}");
+                    break;
+                case "PullRequestEvent":
+                    if (e.payload.Action == "opened")
+                    {
+                        Console.WriteLine($"- Pull requested in {e.Repo.Name}");
+                    }
+                    break;
+                case "PushEvent":
+                    Console.WriteLine($"- Pushed {e.payload.Size} commits to {e.Repo.Name}");
+                    break;
+                case "ReleaseEvent":
+                    Console.WriteLine($"- Published in {e.Repo.Name}");
+                    break;
+                case "SponsorshipEvent":
+                    break;
+                case "WatchEvent":
+                    Console.WriteLine($"- Starred {e.Repo.Name}");
+                    break;
+
+            }
         }
     }
+
 }
 
